7c444f46bd3c21338264790b4638bbc20f75e4e7
sudimanw <dodosudi@yahoo.co.id>
2023-05-30 22:43:16 +0700

    Perapihan Code Backend Menu Tampil Foto

--- resources/views/backends/backend-navigation.blade.php
+++ resources/views/backends/backend-navigation.blade.php
@@ -63,7 +63,11 @@
         </div>
       </li> -->
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="{{ asset('themes-backend/assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+      @if( !empty (Auth::user()->foto) )
+      <img alt="image" src="{{  url('/images-foto/'.Auth::user()->foto) }}" class="rounded-circle mr-1">
+      @else
+      <img alt="image" src="{{ asset('themes-backend/assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+      @endif
         <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           <div class="dropdown-title">Online</div>
