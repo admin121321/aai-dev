08d10ac356204ef28b947267cba5ef0ca4d83753
sudimanw <dodosudi@yahoo.co.id>
2023-05-30 22:02:27 +0700

    Perapihan Code Welcome

---
+++ resources/views/layouts/welcome-css.blade.php
@@ -0,0 +1,17 @@
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="{{ asset('themes-frontend/scripts/bootstrap/bootstrap.min.css') }}">
+		<!-- IonIcons -->
+		<link rel="stylesheet" href="{{ asset('themes-frontend/scripts/ionicons/css/ionicons.min.css') }}">
+		<!-- Toast -->
+		<link rel="stylesheet" href="{{ asset('themes-frontend/scripts/toast/jquery.toast.min.css') }}">
+		<!-- OwlCarousel -->
+		<link rel="stylesheet" href="{{ asset('themes-frontend/scripts/owlcarousel/dist/assets/owl.carousel.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('themes-frontend/scripts/owlcarousel/dist/assets/owl.theme.default.min.css') }}">
+		<!-- Magnific Popup -->
+		<link rel="stylesheet" href="{{ asset('themes-frontend/scripts/magnific-popup/dist/magnific-popup.css') }}">
+		<link rel="stylesheet" href="{{ asset('themes-frontend/scripts/sweetalert/dist/sweetalert.css') }}">
+		<!-- Custom style -->
+		<link rel="stylesheet" href="{{ asset('themes-frontend/css/style.css') }}">
+		<link rel="stylesheet" href="{{ asset('themes-frontend/css/skins/all.css') }}">
+		<link rel="stylesheet" href="{{ asset('themes-frontend/css/demo.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes-frontend/css/safari.css') }}">
---
+++ resources/views/layouts/welcome-js.blade.php
@@ -0,0 +1,13 @@
+    <!-- Scripts -->
+    <script src="{{ asset('themes-frontend/js/jquery.js') }}"></script>
+    <script src="{{ asset('themes-frontend/js/jquery.migrate.js') }}"></script>
+    <script src="{{ asset('themes-frontend/scripts/bootstrap/bootstrap.min.js') }}"></script>
+    <script>var $target_end=$(".best-of-the-week");</script>
+    <script src="{{ asset('themes-frontend/scripts/jquery-number/jquery.number.min.js') }}"></script>
+    <script src="{{ asset('themes-frontend/scripts/owlcarousel/dist/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('themes-frontend/scripts/magnific-popup/dist/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('themes-frontend/scripts/easescroll/jquery.easeScroll.js') }}"></script>
+    <script src="{{ asset('themes-frontend/scripts/sweetalert/dist/sweetalert.min.js') }}"></script>
+    <script src="{{ asset('themes-frontend/scripts/toast/jquery.toast.min.js') }}"></script>
+    <script src="{{ asset('themes-frontend/js/demo.js') }}"></script>
+    <script src="{{ asset('themes-frontend/js/e-magz.js') }}"></script>
