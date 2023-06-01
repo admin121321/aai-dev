ffaa71926d1f833a6b10a11e0c8aa2f90a9293f3
sudimanw <dodosudi@yahoo.co.id>
2023-06-01 20:41:15 +0700

    Export Excel User

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -6,31 +6,50 @@ use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TicketExport implements FromCollection, WithHeadings
+class UserExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    // public function collection()
-    // {
-    //     $tickets = Ticket::join('users', 'users.id', '=' ,'tickets.user_id')
-    //                  ->join('categories', 'categories.id', '=' ,'tickets.category_id')
-    //                  ->select('users.name', 'categories.nama', 'ticket_id', 'title', 'priority', 'message', 'status') 
-    //                  ->orderBy('created_at', 'DESC');
-    //                 //  ->paginate(10);
+    // getCustome dari Model
+    public function collection()
+    {
+        $records = User::select("users.id_anggota", "users.name", "users.t_lahir", "users.tgl_lahir",
+                    "users.j_k", "users.no_telpn", "users.email", "users.kategori", "users.inst",
+                    "users.nrk", "users.nip", "users.no_ser_kar", "users.pang", "users.gol",
+                    "users.tmt_pang", "users.ting", "users.tmt_ting", "users.u_k", "users.level",
+                    "users.is_admin", "users.verifikasi", "users.created_at", "users.updated_at",)
+                    ->get();
 
-    //     return collect($tickets);
-    // }
-
+        return collect($records);
+    }
+    
     public function headings(): array
     {
         return [
-            'Nama Pemohon','Advokat','Kategori', 'ID Tiket', 'Judul', 'Prioritas', 'Pesan', 'Status', 'Tgl'
+            "ID Anggota", 
+            "Nama",
+            "Tempat Lahir", 
+            "Tanggal Lahir", 
+            "Jenis Kelamin", 
+            "No Handphone", 
+            "Email", 
+            "Kategori Pekerjaan", 
+            "Institusi/Perusahaan/Perguruan Tinggi",  
+            "NIP / NIK", 
+            "NRK",
+            "No. Seri Karpeg", 
+            "Pangkat", 
+            "Golongan", 
+            "TMT Pangkat", 
+            "Tingkatan", 
+            "TMT Tingkatan", 
+            "Unit Kerja", 
+            "Level Akses", 
+            "Level Konsul", 
+            "Verifikasi Akun", 
+            "Created at", 
+            "Updated at",
         ];
-    }
-
-    public function collection()
-    {
-        return User::getCustom();
     }
 }
