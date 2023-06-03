cc00488e12becd3f8f658b9c85d6073275f96abc
sudimanw <dodosudi@yahoo.co.id>
2023-06-03 12:48:48 +0700

    Typo buku petunjuk

--- resources/views/backends/backend-sidebar-menu.blade.php
+++ resources/views/backends/backend-sidebar-menu.blade.php
@@ -180,7 +180,7 @@
         </ul>
 
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-        <a href="{ url('/buku-petunjuk') }}" class="btn btn-primary btn-lg btn-block btn-icon-split">
+        <a href="{{ url('/buku-petunjuk') }}" class="btn btn-primary btn-lg btn-block btn-icon-split">
             <i class="fas fa-rocket"></i> Buku Petunjuk
         </a>
         </div>
--- resources/views/users/buku-petunjuk.blade.php
+++ resources/views/users/buku-petunjuk.blade.php
@@ -5,11 +5,11 @@
     <section class="">
         <div class="card">    
             <div class="card-header">
-                <h3>List Anggota</h3>
+                <h3>Buku Petunjuk</h3>
             </div>
         </div>
         <div class="card card-body">
-            <embed type="application/pdf" src="{{ asset('themes-frontend/pdf/buku=petunjuk.pdf') }}" width="300" height="500"></embed>
+            <embed type="application/pdf" src="{{ asset('themes-frontend/pdf/buku-petunjuk.pdf') }}" width="300" height="500"></embed>
         </div>
     </section>
 </div>
