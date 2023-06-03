2d439593a0d0b062ab650bae1ca280af25e44a4b
sudimanw <dodosudi@yahoo.co.id>
2023-06-03 13:01:05 +0700

    Size Buku Petunjuk

--- resources/views/users/buku-petunjuk.blade.php
+++ resources/views/users/buku-petunjuk.blade.php
@@ -9,7 +9,7 @@
             </div>
         </div>
         <div class="card card-body">
-            <embed type="application/pdf" src="{{ asset('themes-frontend/pdf/buku-petunjuk.pdf') }}" width="300" height="500"></embed>
+            <embed type="application/pdf" src="{{ asset('themes-frontend/pdf/buku-petunjuk.pdf') }}" width="100%" height="500"></embed>
         </div>
     </section>
 </div>
